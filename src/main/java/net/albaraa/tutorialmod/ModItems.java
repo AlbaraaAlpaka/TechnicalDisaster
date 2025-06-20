eec02d37b8c042d10c140a632986bbc7400f28f7
AlbaraaAlpaka <105862881+AlbaraaAlpaka@users.noreply.github.com>
2025-06-20 10:43:00 +0200

    first commit

---
+++ src/main/java/net/albaraa/tutorialmod/ModItems.java
@@ -0,0 +1,23 @@
+package net.albaraa.tutorialmod;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SILICON = ITEMS.register("silicon",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_SILICON = ITEMS.register("raw_silicon",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+}
